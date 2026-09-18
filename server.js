e16c7cef89b4e96ffff4020946df3d4485c7d1b3
qwqovo08musk <qwqovo08@gmail.com>
2026-09-18 11:27:48 +0800

    Implement CORS support in server.js
    Added CORS middleware to allow requests from the GitHub Pages frontend.

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const app = express();
 
 app.use(express.json());
 
+// Allow requests from the GitHub Pages frontend
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
